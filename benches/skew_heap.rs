2ff0a565cd9d65ab5c85b3f8c1f7d536ad7e3e2a
Yusuke Nojima <nojima@ynojima.com>
2024-08-22 20:57:26 +0900

    Exclude time of destructor

--- benches/skew_heap.rs
+++ benches/skew_heap.rs
@@ -1,3 +1,4 @@
+use divan::Bencher;
 use skew_heap::SkewHeap;
 use std::collections::BinaryHeap;
 
@@ -53,38 +54,39 @@ type Entry = [u64; 5];
 /*
 Benchmark results:
 
-Timer precision: 20 ns
+Timer precision: 10 ns
 skew_heap                   fastest       │ slowest       │ median        │ mean          │ samples │ iters
 ╰─ push_pop_bench                         │               │               │               │         │
    ├─ BinaryHeap<[u64; 5]>                │               │               │               │         │
-   │  ├─ 100000             15.95 ms      │ 19.27 ms      │ 16.19 ms      │ 16.42 ms      │ 20      │ 20
-   │  ├─ 200000             37.87 ms      │ 49.84 ms      │ 39.71 ms      │ 41 ms         │ 20      │ 20
-   │  ├─ 300000             63.76 ms      │ 99.69 ms      │ 73.67 ms      │ 73.87 ms      │ 20      │ 20
-   │  ├─ 400000             94.08 ms      │ 117.7 ms      │ 101.5 ms      │ 103.5 ms      │ 20      │ 20
-   │  ├─ 500000             129.9 ms      │ 171.4 ms      │ 137.7 ms      │ 142.4 ms      │ 20      │ 20
-   │  ├─ 600000             180.6 ms      │ 328.4 ms      │ 240.1 ms      │ 240.9 ms      │ 20      │ 20
-   │  ├─ 700000             257.5 ms      │ 369.5 ms      │ 289 ms        │ 292.8 ms      │ 20      │ 20
-   │  ╰─ 800000             314.8 ms      │ 422.1 ms      │ 364.4 ms      │ 359.6 ms      │ 20      │ 20
+   │  ├─ 100000             11.82 ms      │ 13.25 ms      │ 12.14 ms      │ 12.18 ms      │ 20      │ 20
+   │  ├─ 200000             26.98 ms      │ 36.36 ms      │ 27.14 ms      │ 28.2 ms       │ 20      │ 20
+   │  ├─ 300000             42.91 ms      │ 51.76 ms      │ 43.09 ms      │ 44.17 ms      │ 20      │ 20
+   │  ├─ 400000             61.6 ms       │ 87.01 ms      │ 65.19 ms      │ 67.37 ms      │ 20      │ 20
+   │  ├─ 500000             81.55 ms      │ 111.3 ms      │ 89.29 ms      │ 91.71 ms      │ 20      │ 20
+   │  ├─ 600000             102.3 ms      │ 136.3 ms      │ 110.7 ms      │ 112.9 ms      │ 20      │ 20
+   │  ├─ 700000             123.1 ms      │ 139.7 ms      │ 127.7 ms      │ 129.2 ms      │ 20      │ 20
+   │  ╰─ 800000             148.6 ms      │ 215.2 ms      │ 155.9 ms      │ 161.7 ms      │ 20      │ 20
    ╰─ SkewHeap<[u64; 5]>                  │               │               │               │         │
-      ├─ 100000             57.09 ms      │ 78.17 ms      │ 65.52 ms      │ 66.26 ms      │ 20      │ 20
-      ├─ 200000             156.9 ms      │ 237.4 ms      │ 173.1 ms      │ 182.2 ms      │ 20      │ 20
-      ├─ 300000             278.8 ms      │ 381.1 ms      │ 308.7 ms      │ 318.2 ms      │ 20      │ 20
-      ├─ 400000             438 ms        │ 572.9 ms      │ 471.2 ms      │ 482.6 ms      │ 20      │ 20
-      ├─ 500000             593.5 ms      │ 763.5 ms      │ 642.5 ms      │ 650.6 ms      │ 20      │ 20
-      ├─ 600000             806 ms        │ 968.8 ms      │ 847.7 ms      │ 848.8 ms      │ 20      │ 20
-      ├─ 700000             1.055 s       │ 1.248 s       │ 1.117 s       │ 1.135 s       │ 20      │ 20
-      ╰─ 800000             1.248 s       │ 1.682 s       │ 1.441 s       │ 1.443 s       │ 20      │ 20
+      ├─ 100000             28.4 ms       │ 33.02 ms      │ 29.05 ms      │ 29.22 ms      │ 20      │ 20
+      ├─ 200000             68.43 ms      │ 93.57 ms      │ 69.36 ms      │ 71.66 ms      │ 20      │ 20
+      ├─ 300000             110.9 ms      │ 136.9 ms      │ 114.3 ms      │ 118.8 ms      │ 20      │ 20
+      ├─ 400000             171.7 ms      │ 229.7 ms      │ 182.2 ms      │ 189.7 ms      │ 20      │ 20
+      ├─ 500000             246.2 ms      │ 296.1 ms      │ 261.5 ms      │ 264.2 ms      │ 20      │ 20
+      ├─ 600000             340.5 ms      │ 386.5 ms      │ 362.4 ms      │ 363.4 ms      │ 20      │ 20
+      ├─ 700000             436.7 ms      │ 509.4 ms      │ 452.5 ms      │ 458.2 ms      │ 20      │ 20
+      ╰─ 800000             549.8 ms      │ 640.1 ms      │ 562.2 ms      │ 572.6 ms      │ 20      │ 20
 */
 #[divan::bench(
     types = [BinaryHeap<Entry>, SkewHeap<Entry>],
     args = [100000, 200000, 300000, 400000, 500000, 600000, 700000, 800000],
     sample_count = 20,
 )]
-fn push_pop_bench<H: Heap<Entry>>(n: u64) {
-    divan::black_box(push_pop(n, H::new()));
+fn push_pop_bench<H: Heap<Entry>>(bencher: Bencher, n: u64) {
+    let mut heap = H::new();
+    bencher.bench_local(|| push_pop(n, &mut heap));
 }
 
-fn push_pop<H: Heap<Entry>>(n: u64, mut heap: H) {
+fn push_pop<H: Heap<Entry>>(n: u64, heap: &mut H) {
     for _ in 0..n {
         heap.push(rand::random::<Entry>());
     }
